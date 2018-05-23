1e8e91c36a0aeb36e247c04d592bb4858a4825a3
unknown <ympaik@hotmail.com>
2018-05-23 21:48:34 +0900

    hello world example

---
+++ workspace/Chapter01/src/Example01.java
@@ -0,0 +1,20 @@
+/* 
+ * 여러
+ * 줄
+ * 짜리
+ * 주석
+ * */
+
+public class Example01 {
+
+	public static void main(String[] args) {
+		
+		// method를 호출 - hello world!! 라는 문자열을 콘솔에 출력
+		System.out.println("Hello World!!!");
+		
+//		System.out.println("Bye World");
+		System.out.println("Greeting World!");
+
+	}
+
+}
