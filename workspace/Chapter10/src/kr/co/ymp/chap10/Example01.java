244f3e7e451ce7fe14e4af98ccec64b24e9fbd51
unknown <ympaik@hotmail.com>
2018-06-05 20:51:13 +0900

    add BallPen example

--- workspace/Chapter10/src/kr/co/ymp/chap10/Example01.java
+++ workspace/Chapter10/src/kr/co/ymp/chap10/Example01.java
@@ -1,7 +1,7 @@
 package kr.co.ymp.chap10;
 
 import kr.co.ymp.chap10.example.Writer;
-
+import kr.co.ymp.chap10.example.BallPen;
 import kr.co.ymp.chap10.example.Pencil;
 
 public class Example01 {
@@ -17,7 +17,39 @@ public class Example01 {
 		writer.refill();
 		
 		pencil.foo();
-		writer.foo();
+		// writer.foo(); --> 에러
+		
+		// 필기구는 연필이다 x
+//		Pencil p = new Writer(); --> 에러
+		
+		// 이 필기구는 연필이다. o
+		// Downcasting
+		Pencil p = (Pencil)writer;
+		p.foo();
+		
+		BallPen ballpen = new BallPen(2000, "빨간색");
+		ballpen.write("필기필기");
+		ballpen.refillBallPenColor("파랑색");
+		ballpen.write("바꾸고 나서 필기하기");
+		
+		Writer writer2 = new BallPen(1500, "빨강");
+		
+//		Pencil pp = (Pencil)writer2; --> 에러. 볼팬은 연필로 형 변환 불가
+//		pp.foo();
+		
+		if(writer instanceof Pencil) {
+			System.out.println("이 필기구는 연필 입니다.");
+			Pencil pp = (Pencil)writer;
+			pp.foo();
+		}
+		
+		if(writer2 instanceof Pencil) {
+			System.out.println("이 필기구는 연필 입니다.");
+			Pencil pp = (Pencil)writer;
+			pp.foo();
+		}else {
+			System.out.println("이 필기구는 연필이 아닙니다.");
+		}
 		
 	}
 }
--- workspace/Chapter10/src/kr/co/ymp/chap10/example/BallPen.java
+++ workspace/Chapter10/src/kr/co/ymp/chap10/example/BallPen.java
@@ -1,5 +1,22 @@
 package kr.co.ymp.chap10.example;
 
-public class BallPen {
-
+public class BallPen extends Writer {
+	
+	public BallPen(int price, String color) {
+		super(price, color);
+	}
+	
+	public void setColor(String color) {
+		super.color = color;
+	}
+	
+	public void refill() {
+		System.out.println(super.color + " 의 볼펜심지를 바꿉니다.");
+	}
+	
+	public void refillBallPenColor(String color) {
+		setColor(color);
+		refill();
+	}
+	
 }
